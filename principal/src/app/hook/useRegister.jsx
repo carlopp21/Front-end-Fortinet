e9216f7720b96669cb161267cdd65d0c9cacaf25
carlopp21 <carlitosmancipito21@gmail.com>
2025-08-12 11:31:10 -0500

    Cambio .trim

--- principal/src/app/hook/useRegister.jsx
+++ principal/src/app/hook/useRegister.jsx
@@ -36,7 +36,7 @@ export default function useRegister() {
             numeroTelefono: numeroTelefono.trim(),
             correoUsuario: correoUsuario.trim(),
             nitEmpresa: nitEmpresa.trim(),
-            tipoLicencia: tipoLicencia.trim()
+            tipoLicencia: tipoLicencia
         }
         try {
             const baseUrl = process.env.NEXT_PUBLIC_API_URL;
